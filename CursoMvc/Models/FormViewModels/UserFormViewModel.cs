03282fa5d853729b6f5770a2c0888747f7b43868
Mikel Lavin Dobaran <mlavind@gmail.com>
2022-02-16 00:27:35 +0100

    Proyecto MVC Entity framework: Agregar modelo formulario add

---
+++ CursoMvc/Models/FormViewModels/UserFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CursoMvc.Models.FormViewModels
+{
+    public class UserFormViewModel
+    {
+        public string Email { get; set;}
+        public string Password { get; set;}
+        public string ConfirmPassword { get; set;}
+        public int Edad { get; set; }
+
+    }
+}
